ff2151c64ba9d37a1ca5d3ddad487d3c3bd8c154
Pierre Pronchery <khorben@defora.org>
2013-06-04 02:05:24 +0200

    Fixed path to c99

--- tools/graph.sh
+++ tools/graph.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$Id$
-#Copyright (c) 2010-2012 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2010-2013 Pierre Pronchery <khorben@defora.org>
 #This file is part of DeforaOS Unix utils
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 
 
 #variables
-CC="../../../../Devel/src/c99/src/c99"
+CC="../../../../../Devel/src/c99/C99-git/src/c99"
 DOT="dot -Tpng:cairo:gd"
 RM="rm -f"
 VIEW="view"
