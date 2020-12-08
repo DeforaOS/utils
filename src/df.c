87efa6602b0dc583a13e9faf4dd636f1afffa7f2
Pierre Pronchery <khorben@defora.org>
2020-12-08 19:44:14 +0100

    Update the copyright notice

--- src/df.c
+++ src/df.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2006-2013 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2006-2020 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Unix utils */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
