18bca6d99b51ca5b6254cfa87cd2fff5dd94c8a3
Pierre Pronchery <khorben@defora.org>
2020-12-08 14:18:06 +0100

    Update the copyright notice

--- tools/utils.sh
+++ tools/utils.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$Id$
-#Copyright (c) 2009-2017 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2009-2020 Pierre Pronchery <khorben@defora.org>
 #This file is part of DeforaOS Unix utils
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
