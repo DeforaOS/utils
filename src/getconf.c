1f168930d7c12b0c5e491dc928ae35b87123d1b1
Pierre Pronchery <khorben@defora.org>
2020-12-21 04:18:45 +0100

    getconf: register additional sysconf() variables

--- src/getconf.c
+++ src/getconf.c
@@ -48,65 +48,110 @@ static const getconf_catalog _getconf_catalog_confstr[] =
 
 static const getconf_catalog _getconf_catalog_limits[] =
 {
+#ifdef _SC_2_C_BIND
+	{ _SC_2_C_BIND,		"_POSIX2_C_BIND"	},
+#endif
+#ifdef _SC_2_C_DEV
+	{ _SC_2_C_DEV,		"_POSIX2_C_DEV"		},
+#endif
+#ifdef _SC_2_CHAR_TERM
+	{ _SC_2_CHAR_TERM,	"_POSIX2_CHAR_TERM"	},
+#endif
+#ifdef _SC_2_FORT_DEV
+	{ _SC_2_FORT_DEV,	"_POSIX2_FORT_DEV"	},
+#endif
+#ifdef _SC_2_FORT_RUN
+	{ _SC_2_FORT_RUN,	"_POSIX2_FORT_RUN"	},
+#endif
+#ifdef _SC_2_LOCALEDEF
+	{ _SC_2_LOCALEDEF,	"_POSIX2_LOCALEDEF"	},
+#endif
+#ifdef _SC_2_PBS
+	{ _SC_2_PBS,		"_POSIX2_PBS"		},
+#endif
+#ifdef _SC_2_PBS_ACCOUNTING
+	{ _SC_2_PBS_ACCOUNTING,	"_POSIX2_PBS_ACCOUNTING"},
+#endif
+#ifdef _SC_2_PBS_CHECKPOINT
+	{ _SC_2_PBS_CHECKPOINT,	"_POSIX2_PBS_CHECKPOINT"},
+#endif
+#ifdef _SC_2_PBS_LOCATE
+	{ _SC_2_PBS_LOCATE,	"_POSIX2_PBS_LOCATE"	},
+#endif
+#ifdef _SC_2_PBS_MESSAGE
+	{ _SC_2_PBS_MESSAGE,	"_POSIX2_PBS_MESSAGE"	},
+#endif
+#ifdef _SC_2_PBS_TRACK
+	{ _SC_2_PBS_TRACK,	"_POSIX2_PBS_TRACK"	},
+#endif
+#ifdef _SC_2_SW_DEV
+	{ _SC_2_SW_DEV,		"_POSIX2_SW_DEV"	},
+#endif
+#ifdef _SC_2_UPE
+	{ _SC_2_UPE,		"_POSIX2_UPE"		},
+#endif
+#ifdef _SC_2_VERSION
+	{ _SC_2_VERSION,	"_POSIX2_VERSION"	},
+#endif
 #ifdef _SC_AIO_LISTIO_MAX
-	{ _SC_AIO_LISTIO_MAX,	"AIO_LISTIO_MAX"},
+	{ _SC_AIO_LISTIO_MAX,	"AIO_LISTIO_MAX"	},
 #endif
 #ifdef _SC_AIO_MAX
-	{ _SC_AIO_MAX,		"AIO_MAX"	},
+	{ _SC_AIO_MAX,		"AIO_MAX"		},
 #endif
 #ifdef _SC_ARG_MAX
-	{ _SC_ARG_MAX,		"ARG_MAX"	},
+	{ _SC_ARG_MAX,		"ARG_MAX"		},
 #endif
 #ifdef _SC_ATEXIT_MAX
-	{ _SC_ATEXIT_MAX,	"ATEXIT_MAX"	},
+	{ _SC_ATEXIT_MAX,	"ATEXIT_MAX"		},
 #endif
 #ifdef _SC_CHILD_MAX
-	{ _SC_CHILD_MAX,	"CHILD_MAX"	},
+	{ _SC_CHILD_MAX,	"CHILD_MAX"		},
 #endif
 #ifdef _SC_DELAYTIMER_MAX
-	{ _SC_DELAYTIMER_MAX,	"DELAYTIMER_MAX"},
+	{ _SC_DELAYTIMER_MAX,	"DELAYTIMER_MAX"	},
 #endif
 #ifdef _SC_EXPR_NEST_MAX
-	{ _SC_EXPR_NEST_MAX,	"EXPR_NEST_MAX"	},
+	{ _SC_EXPR_NEST_MAX,	"EXPR_NEST_MAX"		},
 #endif
 #ifdef _SC_HOST_NAME_MAX
-	{ _SC_HOST_NAME_MAX,	"HOST_NAME_MAX"	},
+	{ _SC_HOST_NAME_MAX,	"HOST_NAME_MAX"		},
 #endif
 #ifdef _SC_IOV_MAX
-	{ _SC_IOV_MAX,		"IOV_MAX"	},
+	{ _SC_IOV_MAX,		"IOV_MAX"		},
 #endif
 #ifdef _SC_LINE_MAX
-	{ _SC_LINE_MAX,		"LINE_MAX"	},
+	{ _SC_LINE_MAX,		"LINE_MAX"		},
 #endif
 #ifdef _SC_LOGIN_NAME_MAX
-	{ _SC_LOGIN_NAME_MAX,	"LOGIN_NAME_MAX"},
+	{ _SC_LOGIN_NAME_MAX,	"LOGIN_NAME_MAX"	},
 #endif
 #ifdef _SC_NGROUPS_MAX
-	{ _SC_NGROUPS_MAX,	"NGROUPS_MAX"	},
+	{ _SC_NGROUPS_MAX,	"NGROUPS_MAX"		},
 #endif
 #ifdef _SC_OPEN_MAX
-	{ _SC_OPEN_MAX,		"OPEN_MAX"	},
+	{ _SC_OPEN_MAX,		"OPEN_MAX"		},
 #endif
 #ifdef _SC_PAGE_SIZE
-	{ _SC_PAGE_SIZE,	"PAGE_SIZE"	},
+	{ _SC_PAGE_SIZE,	"PAGE_SIZE"		},
 #endif
 #ifdef _SC_PAGESIZE
-	{ _SC_PAGESIZE,		"PAGESIZE"	},
+	{ _SC_PAGESIZE,		"PAGESIZE"		},
 #endif
 #ifdef _SC_STREAM_MAX
-	{ _SC_STREAM_MAX,	"STREAM_MAX"	},
+	{ _SC_STREAM_MAX,	"STREAM_MAX"		},
 #endif
 #ifdef _SC_SYMLOOP_MAX
-	{ _SC_SYMLOOP_MAX,	"SYMLOOP_MAX"	},
+	{ _SC_SYMLOOP_MAX,	"SYMLOOP_MAX"		},
 #endif
 #ifdef _SC_TIMER_MAX
-	{ _SC_TIMER_MAX,	"TIMER_MAX"	},
+	{ _SC_TIMER_MAX,	"TIMER_MAX"		},
 #endif
 #ifdef _SC_TTYNAME_MAX
-	{ _SC_TTYNAME_MAX,	"TTYNAME_MAX"	},
+	{ _SC_TTYNAME_MAX,	"TTYNAME_MAX"		},
 #endif
 #ifdef _SC_TZNAME_MAX
-	{ _SC_TZNAME_MAX,	"TZNAME_MAX"	},
+	{ _SC_TZNAME_MAX,	"TZNAME_MAX"		},
 #endif
 };
 
